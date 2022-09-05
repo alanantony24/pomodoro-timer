7c807ccad500138dd486ee5bbc128258ec87992a
leclerc162003 <S10203330@connect.np.edu.sg>
2022-09-05 09:55:45 +0800

    Added Start Pause Toggle to Button

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const timer = {
     longBreakInterval: 4,
   };
 //Start
+var start = false 
+var remainingMins = 0
+var remainingSeconds = 0
 var mins = 25
 var seconds = 0
 var formattedseconds = ("0" + seconds).slice(-2);
@@ -34,27 +37,41 @@ function LongBreakMins() {
   }
 
 function Start(){
-    interval = setInterval(function() {
+    //document.getElementsByClassName("toggleStartPause")[0].textContent = "Pause"
+    if(!start){
+        start = true
+        document.getElementsByClassName("toggleStartPause")[0].textContent = "Pause"
+        interval = setInterval(function() {
+    
+                if(!seconds){
+                    mins--
+                    if(mins < 0){
+                        clearInterval(interval); 
+                        mins = 0
+                        seconds = 1 // 1 as once it reaches 0, it will minus 1 again 
+                        alert("Done");
+                    }
+                    else{
+                        seconds = 60
+                    }
+                }
+                seconds--;
+                var formattedseconds = ("0" + seconds).slice(-2);
+                document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
+                remainingMins = mins
+                remainingSeconds = seconds
+        },1000)
+    } else{
+        clearTimeout(interval)
+        start = false
+        document.getElementsByClassName("toggleStartPause")[0].textContent = "Start"
+        document.getElementById("timerText").innerHTML = remainingMins + ":" + ("0" + remainingSeconds).slice(-2);
+    }
 
-            if(!seconds){
-                mins--
-                if(mins < 0){
-                    clearInterval(interval); 
-                    mins = 0
-                    seconds = 1 // 1 as once it reaches 0, it will minus 1 again 
-                    alert("Done");
-                }
-                else{
-                    seconds = 60
-                }
-            }
-            seconds--;
-            var formattedseconds = ("0" + seconds).slice(-2);
-            document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
-    },1000)
 }
 
 function Reset(){
+    document.getElementById("timerText").innerHTML = mins + ":" + ("0" + seconds).slice(-2);
     clearInterval(interval); 
     mins = 25
     seconds = 0
