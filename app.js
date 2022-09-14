791f8ac8d072b27fd24456debbb6d594303bccdd
Alan Antony James <s10207327@connect.np.edu.sg>
2022-09-14 15:41:41 +0800

    fix reset issue

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ const timer = {
     shortBreak: 5,
     longBreak: 15,
     longBreakInterval: 4,
-  };
+};
+
 //Start
-var start = false 
+var start = false
 var remainingMins = 0
 var remainingSeconds = 0
 var mins = 25
 var seconds = 0
 var formattedseconds = ("0" + seconds).slice(-2);
+
 document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
-function PomodoroMins(){
+
+function PomodoroMins() {
     mins = 25
     seconds = 0
     var formattedseconds = ("0" + seconds).slice(-2);
     document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
-  }
+}
 
 function ShortBreakMins() {
     mins = 5
@@ -34,34 +37,34 @@ function LongBreakMins() {
     seconds = 0
     var formattedseconds = ("0" + seconds).slice(-2);
     document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
-  }
+}
 
-function Start(){
+function Start() {
     //document.getElementsByClassName("toggleStartPause")[0].textContent = "Pause"
-    if(!start){
+    if (!start) {
         start = true
         document.getElementsByClassName("toggleStartPause")[0].textContent = "Pause"
-        interval = setInterval(function() {
-    
-                if(!seconds){
-                    mins--
-                    if(mins < 0){
-                        clearInterval(interval); 
-                        mins = 0
-                        seconds = 1 // 1 as once it reaches 0, it will minus 1 again 
-                        alert("Done");
-                    }
-                    else{
-                        seconds = 60
-                    }
+        interval = setInterval(function () {
+
+            if (!seconds) {
+                mins--
+                if (mins < 0) {
+                    clearInterval(interval);
+                    mins = 0
+                    seconds = 1 // 1 as once it reaches 0, it will minus 1 again 
+                    alert("Done");
                 }
-                seconds--;
-                var formattedseconds = ("0" + seconds).slice(-2);
-                document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
-                remainingMins = mins
-                remainingSeconds = seconds
-        },1000)
-    } else{
+                else {
+                    seconds = 60
+                }
+            }
+            seconds--;
+            var formattedseconds = ("0" + seconds).slice(-2);
+            document.getElementById("timerText").innerHTML = mins + ":" + formattedseconds;
+            remainingMins = mins
+            remainingSeconds = seconds
+        }, 1000)
+    } else {
         clearTimeout(interval)
         start = false
         document.getElementsByClassName("toggleStartPause")[0].textContent = "Start"
@@ -70,9 +73,9 @@ function Start(){
 
 }
 
-function Reset(){
-    document.getElementById("timerText").innerHTML = mins + ":" + ("0" + seconds).slice(-2);
-    clearInterval(interval); 
+function Reset() {
+    clearInterval(interval);
     mins = 25
     seconds = 0
+    document.getElementById("timerText").innerHTML = mins + ":" + ("0" + seconds).slice(-2);
 }
